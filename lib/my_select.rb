aad12dec264a5f0fad5b3f4b946161ced3195801
Robert Peck <rspeckpfs@gmail.com>
2017-08-30 22:40:23 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
- # your code here!
+ i = 0
+ output = []
+  while i < collection.length
+    truefalse = yield collection[i] #Theres probably a more concise way to do this.
+    if truefalse
+      output << collection[i]
+    end
+    i = i + 1
+  end
+  output
 end
